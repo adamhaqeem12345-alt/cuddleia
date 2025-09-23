c529174d6c222b585681b0a82324bb3b0e3d962e
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-23 04:22:28 +0000

    Can you put example file in the database to ease me?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import { Hero } from '@/components/home/hero';
 import { ProductShowcase } from '@/components/home/product-showcase';
 import { products } from '@/lib/products';
 
-
 export default async function Home() {
-  
   return (
     <div className="flex flex-col min-h-dvh">
       <main className="flex-1">
