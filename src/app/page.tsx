aa2a1dd02c4addffec2a72734340d28fd3fe8948
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-23 04:09:29 +0000

    How to put my products?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,10 @@
 
 import { Hero } from '@/components/home/hero';
 import { ProductShowcase } from '@/components/home/product-showcase';
-import { db } from '@/lib/firebase';
-import { Product } from '@/lib/products';
-import { collection, getDocs } from 'firebase/firestore';
-
-async function getProducts(): Promise<Product[]> {
-  const productsCol = collection(db, 'products');
-  const productSnapshot = await getDocs(productsCol);
-  const productList = productSnapshot.docs.map(doc => {
-    const data = doc.data();
-    return {
-      id: doc.id,
-      name: data.name,
-      description: data.description,
-      price: data.price,
-      category: data.category,
-      imageUrl: data.imageUrl,
-    };
-  });
-  return productList as Product[];
-}
+import { products } from '@/lib/products';
 
 
 export default async function Home() {
-  const products = await getProducts();
   
   return (
     <div className="flex flex-col min-h-dvh">
