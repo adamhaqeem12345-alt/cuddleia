a345a0fe5fe8dbcd70cebd28d5440becd582106c
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-24 10:08:52 +0000

    Same

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -69,7 +69,7 @@ export default function AboutPage() {
             className="relative h-96 w-full overflow-hidden rounded-2xl shadow-xl"
           >
             <Image
-              src="https://images.unsplash.com/photo-1565530999933-3820a631f235?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src="https://images.unsplash.com/photo-1565530999933-3820a631f235?q=80&w=1080&auto=format&fit=crop"
               alt="A graphic background of pastel colored flowers."
               fill
               className="object-cover"
