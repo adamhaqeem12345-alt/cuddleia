5b6b7078e45bff39b51a9da21bcbb6dfa5539387
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-24 14:06:52 +0000

    Product Name: iPad Wallpaper (Lock Screen, Landscape) – Allah Loves You

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -27,5 +27,14 @@ export const products: Product[] = [
     category: 'Wallpapers',
     imageUrl: 'https://i.postimg.cc/WbdpVVJV/Islamic-i-Pad-Wallpaper-zip-2.png',
     downloadUrl: 'https://drive.google.com/file/d/1OyatP86tevbHRiazIMEsal09fcFOMSZl/view?usp=drivesdk'
+  },
+  {
+    id: '003',
+    name: 'iPad Wallpaper (Lock Screen, Landscape) – Allah Loves You Forever (Minimalist Series)',
+    description: 'A digital Islamic wallpaper featuring floral background and Arabic calligraphy of Allah and Muhammad ﷺ, with the reminder ‘Allah Loves You\'. ✨It’s made for iPad A16 (2048 × 2732 pixels) and works best as a lock screen in landscape orientation with the normal clock display and no widgets. The wallpaper is high-resolution and clear, suitable for iPad Pro 12.9-inch (3rd Gen and newer), iPad Air 10.9-inch, iPad 10th Gen, iPad Mini 6 (scaled fit), and other tablets with a similar 4:3 aspect ratio. ⚠️ Please note, it’s not recommended for portrait lock screens. After purchase, you will receive a high-quality PNG file sent directly to your email. This is a digital item only (no physical shipping), and it’s for personal use only—not for resale or redistribution.',
+    price: 5.00,
+    category: 'Wallpapers',
+    imageUrl: 'https://i.postimg.cc/25KS03k1/Islamic-i-Pad-Wallpaper-zip-3.png',
+    downloadUrl: 'https://drive.google.com/file/d/17I5xYCRP4wItUYWtGJfWSyusxCoI3FAY/view?usp=drivesdk'
   }
 ];
