13c796b0f428b3b177bccc5491e4fd326cbef8e4
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-25 16:26:30 +0000

    This is the right conversion

--- src/lib/currency.ts
+++ src/lib/currency.ts
@@ -1,4 +1,4 @@
 // This rate is for converting USD to MYR.
-// Example: 1 USD = 4.7 MYR
+// Example: 1 USD = 4.208 MYR
 // You can update this value periodically to reflect the latest exchange rates.
-export const USD_TO_MYR_RATE = 4.7;
+export const USD_TO_MYR_RATE = 4.208;
