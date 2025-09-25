3dcc1d40902cb360b90237b38ab74fc74ee22f0d
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-25 15:39:58 +0000

    Remove contact section

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,7 +17,6 @@ const navLinks = [
     { href: "/", label: "Home" },
     { href: "/products", label: "Products" },
     { href: "/about", label: "About" },
-    { href: "/contact", label: "Contact" },
 ]
 
 const Header = () => {
