7107d237c303fc4044819ef6c78f3da19b177873
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-26 06:41:19 +0000

    Paypal client id: AcP9f98y69e5wW3gR4v1qoIoZejFUNxj4CF9ceA-CBbXq152xI1qnM

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
     NEXT_PUBLIC_URL: process.env.NODE_ENV === 'development' 
       ? 'http://localhost:3000' 
       : 'https://www.cuddleia.com',
-    NEXT_PUBLIC_PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+    NEXT_PUBLIC_PAYPAL_CLIENT_ID: 'AcP9f98y69e5wW3gR4v1qoIoZejFUNxj4CF9ceA-CBbXq152xI1qnMugLF_rKs3yXN-fuyFIKuWpqeIW',
     PAYPAL_CLIENT_SECRET: process.env.PAYPAL_CLIENT_SECRET,
     PAYPAL_EMAIL: 'adamhaqeem12345@gmail.com',
     GMAIL_USER: 'hello@cuddleia.com',
