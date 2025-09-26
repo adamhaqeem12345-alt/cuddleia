6db46966a4b509d22056e30e3857bc7e3ef1d56b
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-26 10:10:05 +0000

    Webhooks id: 67N55929UU247644U

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
     NEXT_PUBLIC_PAYPAL_CLIENT_ID: 'AcP9f98y69e5wW3gR4v1qoIoZejFUNxj4CF9ceA-CBbXq152xI1qnMugLF_rKs3yXN-fuyFIKuWpqeIW',
     PAYPAL_CLIENT_SECRET: 'AcP9f98y69e5wW3gR4v1qoIoZejFUNxj4CF9ceA-CBbXq152xI1qnMugLF_rKs3yXN-fuyFIKuWpqeIW',
     PAYPAL_EMAIL: 'adamhaqeem12345@gmail.com',
-    PAYPAL_WEBHOOK_ID: 'YOUR_WEBHOOK_ID',
+    PAYPAL_WEBHOOK_ID: '67N55929UU247644U',
     GMAIL_USER: 'hello@cuddleia.com',
     GMAIL_APP_PASSWORD: 'ZNFE8nNe0WM4',
     // Switch between 'https://api-m.sandbox.paypal.com' and 'https://api-m.paypal.com'
