e089c0502a14b07eadced1af96d9e07b550e4762
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-28 00:59:43 +0000

    404 not found

--- next.config.js
+++ next.config.js
@@ -9,12 +9,6 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
             },
-            {
-                protocol: 'https',
-                hostname: 'iili.io',
-                port: '',
-                pathname: '/**',
-            }
         ],
     },
 };
