9e74813ecc141a44054b3ccf2eb98119a0903658
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-28 10:11:17 +0000

    You do it

--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
             },
         ],
     },
+    env: {
+        NEXT_PUBLIC_PAYPAL_CLIENT_ID: 'AcP9f98y69e5wW3gR4v1qoIoZejFUNxj4CF9ceA-CBbXq152xI1qnMugLF_rKs3yXN-fuyFIKuWpqeIW',
+    }
 };
 
 module.exports = nextConfig;
