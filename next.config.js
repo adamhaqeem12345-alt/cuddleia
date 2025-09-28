9d75cbbcd8908a70a4096d0b6869921f63f27df1
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-28 19:18:01 +0000

    Failed

--- next.config.js
+++ next.config.js
@@ -10,9 +10,6 @@ const nextConfig = {
             },
         ],
     },
-    env: {
-        NEXT_PUBLIC_PAYPAL_CLIENT_ID: 'AcP9f98y69e5wW3gR4v1qoIoZejFUNxj4CF9ceA-CBbXq152xI1qnMugLF_rKs3yXN-fuyFIKuWpqeIW',
-    },
 };
 
 module.exports = nextConfig;
