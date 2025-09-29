8824cb1d9c7fc14d5ee35dde5bde068b8781b9df
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-29 15:39:32 +0000

    Not fixing the eslint, but modify or disable rules in eslint using this

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// mimic CommonJS variables -- not needed if using Node < 20.11.0
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+];
+
+export default eslintConfig;
