2ca018409d86d8d3a563088d55e80459f3f8f0dc
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-09-30 00:01:09 +0000

    No code change but digest this first: To fix the issue of customers bein

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
