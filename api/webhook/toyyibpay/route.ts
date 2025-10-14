7d8849f643c3057bd93f7897d7a05877514576f4
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-10-14 06:18:37 +0000

    CLEAR ALL FILES BY PHASE

--- api/webhook/toyyibpay/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST() {
-    return NextResponse.json(
-        { message: 'This endpoint is no longer active.' },
-        { status: 410 } // 410 Gone
-    );
-}
