5896dd47d69ac2773f5c7736ddbd53d96cbf0b1f
Adam Haqeem (Futurepilot) <adamhaqeem12345@gmail.com>
2025-10-17 16:04:21 +0000

    Same error

--- next.config.js
+++ next.config.js
@@ -10,11 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    TOYYIBPAY_SECRET_KEY: process.env.TOYYIBPAY_SECRET_KEY,
-    TOYYIBPAY_CATEGORY_CODE: process.env.TOYYIBPAY_CATEGORY_CODE,
-    TOYYIBPAY_API_ENV: process.env.TOYYIBPAY_API_ENV,
-  },
 };
 
 module.exports = nextConfig;
